9a959150d53ca48c215430b8b5d7aeb315e993fc
Cillomir <joel_leckie@hotmail.com>
2023-03-14 19:15:36 -0600

    updates to save player method

--- player.py
+++ player.py
@@ -4,6 +4,7 @@
 # File created: 2023-03-14
 
 import characters
+from json import loads, dumps
 
 player = None
 
@@ -20,7 +21,27 @@ def new_player(name: str):
 # ~~~~~~ Save player                           ~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def save_player(p: characters.PC):
-    f = open("player.txt", "wt")
-    temp = f.read()
-    if temp.contains("Name: " + p.name):
+    try:
+        f = open("player.txt", "wt")
+        temp = loads(f.read())
+        if temp.has_key(p.name):
+            cmd = ''
+            print('That character file already exists.')
+            cmd = input(f'Do you want to overwrite {p.name}? [Y/N] ').lower()
+            while cmd != 'n' and cmd != 'y':
+                print('Please enter a valid option')
+                cmd = input(f'Do you want to overwrite {p.name}? [Y/N] ').lower()
+            if cmd == 'n':
+                return
+        try:
+            temp[p.name] = player
+            f.write(temp)
+        except OSError:
+            print('Unable to write to save file')
+            return
+    except OSError:
+        print('Unable to find or create save file')
+        return
+    finally:
+        f.close()
 
