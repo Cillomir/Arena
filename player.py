5acdfaefe800d2fac00e54a4dbae75de8be5bf9d
Joel Leckie <joel_leckie@hotmail.com>
2023-03-19 14:06:19 -0600

    more updates to player skills and spells

--- player.py
+++ player.py
@@ -14,7 +14,8 @@ from os import system
 
 
 # Class Skills
-# Rage, Bloodthirst, Herald Death, Snipe
+# Rage, Bloodthirst, Herald Death, Snipe, Evasion, Vital Strike, Brutal Strike, Cleave
+# Backstab, Music, Marksman, Slit Throat, Ambush,
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~ Create new player data                ~~~~~
@@ -37,29 +38,34 @@ class PC(Character):
         self.resist = {'fire': 0, 'water': 0, 'earth': 0, 'air': 0, 'cold': 0, 'electric': 0}
         self.resist += {'toxin': 0, 'poison': 0, 'light': 0, 'dark': 0, 'disease': 0}
         # Other Stats (?): Reputation, Luck, Charm/Poise, Knowledge, Personality
-        # Other Stats (?): Sanity, Courage, Constitution, Balance, Vitality
+        # Other Stats (?): Sanity, Courage, Balance
         self.pc_class = None
         self.pc_abilities = None
         # Weapon & Armor Skills:
         self.skill_weapon = {'dagger': 5, 'sword': 0, 'axe': 0, 'hammer': 0, 'staff': 5}
+        self.skill_weapon += {'two-hand sword': 0, 'two-hand axe': 0, 'two-hand hammer': 0, 'two-hand staff': 0}
         self.skill_weapon += {'spear': 0, 'polearm': 0, 'lance': 0, 'whip': 0, 'flail': 0}
-        self.skill_weapon += {'dart': 0, 'thrown': 0, 'bow': 0, 'crossbow': 0}
+        self.skill_weapon += {'dart': 0, 'javelin': 0, 'thrown': 0, 'bow': 0, 'crossbow': 0}
         self.skill_weapon += {'stave': 0, 'wand': 0, 'scroll': 0, 'magic items': 0}
-        self.skill_exotic = {'hook': 0, 'scythe': 0, 'sickle': 0}
+        self.skill_exotic = {'hook': 0, 'scythe': 0, 'sickle': 0, 'bola': 0, 'boomerang': 0}
         self.skill_armor = {'shield': 0, 'light': 0, 'medium': 0, 'heavy': 0}
         # Combat Skills:
         self.skills_attacks = {'second': 0, 'third': 0, 'fourth': 0, 'fifth': 0}
         self.skills_combat = {'hand-to-hand': 0, 'blind-fighting': 0, 'martial-arts': 0, 'enhance damage': 0}
-        self.skills_combat += {'kick': 0, 'bash': 0, 'sweep': 0, 'charge': 0}
+        self.skills_combat += {'kick': 0, 'bash': 0, 'sweep': 0, 'charge': 0, 'grapple': 0, 'feint': 0}
+        self.skills_combat += {'defend': 0, 'slash': 0, 'disarm': 0}
         # Defense Skills:
         self.skill_defense = {'parry': 0, 'dodge': 5, 'block': 0}
         self.skill_defense += {'second defence': 0}
         # Technical Skills:
-        self.skill_technical = {'stealth': 0, 'sneak':0, 'hide': 0, 'scan': 0, 'meditation': 0}
-        self.skill_technical += {'acrobatics': 0, 'athletics': 0}
-        self.skill_technical += {'herbalism': 0, 'scribe': 0}
-        self.skill_passive = {'fast healing': 0, 'perception': 0, 'riding': 0}
+        self.skill_technical = {'stealth': 0, 'sneak': 0, 'hide': 0, 'scan': 0, 'tracking': 0, 'meditation': 0}
+        self.skill_technical += {'acrobatics': 0, 'athletics': 0, 'disarm traps': 0, 'snare': 0}
+        self.skill_technical += {'herbalism': 0, 'scribe': 0, 'peek': 0, 'thievery': 0, 'forgery': 0, 'lock picking': 0, 'pickpocket': 0}
+        self.skill_technical += {'imbue poison': 0}
+        self.skill_passive = {'fast healing': 0, 'perception': 0, 'riding': 0, 'detect traps': 0}
+        self.skill_passive += {}
         self.skill_passive += {'knowledge': 0, 'lore': 0}
+        self.skill_enhancements += {'endurance': 0, 'vitality': 0, 'constitution': 0}
         # Mystic Spells:
         self.spell_mystic = {'lay on hands': 0}
         # Abjuration Spells
@@ -82,7 +88,7 @@ class PC(Character):
         # Create Water
         # Abjuration
         # Abjure 	Counterspell 	Dispel Area 	Dispel Magic
-        # Room Shield 	Silence
+        # Room Shield 	Silence, Fireshield
         # Alteration
         # Change Sex 	Death Grip 	Erase 	Flame Wind
         # Haste 	Infravision 	Lava Walk 	Magic Lock
@@ -140,6 +146,159 @@ class PC(Character):
         # Armor 	Hands Of Wind 	Phalanx
         # Chaos
         # Slow Magic
+        # Thought
+        # Bar 	Cancellation 	Describe 	Disequilibriate
+        # Disrupt Sight 	Domination 	Esp 	Float
+        # Force Field 	Forget 	Geisteblitz 	Induce Aggression
+        # Irritation 	Knock 	Memory Drain 	Mental Disruption
+        # Mesmerize 	Mimic 	Mind Shield 	Nullification Field
+        # Pense 	Pyrokinesis 	Sensory Enhancement 	Spook
+        # Telekinesis 	Telekinetic Bash 	Telekinetic Explosion 	Telekinetic Pierce
+        # Telekinetic Punch 	Telekinetic Shield 	Telekinetic Slash 	Telekinetic Wave
+        # Telesmatic Force 	Teleview 	Transference
+        # Abjuration
+        # Counterspell 	Dispel Magic 	Room Shield
+        # Alteration
+        # Death Grip 	Enhanced Strength 	Erase 	Guise Of Nature
+        # Infravision 	Magic Lock 	Magic Unlock 	Otolithic Growth
+        # Spark 	Stone Skin 	Whirlwind 	Wind Walk
+        # Summoning
+        # Blink 	Control Winds 	Find Familiar 	Ice Storm
+        # Summon Mount
+        # Enchantment
+        # Charm 	Faerie Fire 	Levitation 	Recharge Item
+        # Sleep 	Tinnitus
+        # Illusion
+        # Color Spray 	Concealment 	Invisibility 	Light
+        # Momentary Darkness
+        # Divination
+        # Detect Illusion 	Detect Magic 	Identify 	Sense Life
+        # Wizard Eye
+        # Conjuration
+        # Camouflage 	Circle Of Thorns 	Create Food 	Create Spring
+        # Create Water 	Electrogenic Growth 	Flintstrike 	Frost
+        # Fungal Growth 	Hands Of Wind 	Ice Wind 	Iceball
+        # Magic Bomb 	Magic Dart 	Magic Message 	Web
+        # Abjuration
+        # Abjure 	Awaken 	Condemn 	Counterspell
+        # Cure Disease 	Cure Poison 	Protection From Evil 	Protection From Good
+        # Remove Curse 	Sanctuary 	Tremor
+        # Alteration
+        # Calm 	Change Sex 	Cure Blindness 	Cure Critical
+        # Cure Light 	Erase 	Heal 	Poison
+        # Refresh 	Requiem
+        # Summoning
+        # Bubble Cluster 	Summon 	Word Of Recall
+        # Enchantment
+        # Consecrate Armor 	Consecrate Food 	Excommunicate
+        # Illusion
+        # Light
+        # Invocation
+        # Bless
+        # Evocation
+        # Call Lightning
+        # Divination
+        # Detect Alignment 	Detect Illusion 	Detect Invisibility 	Identify
+        # Read Aura 	Sense Life
+        # Necromancy
+        # Animate Dead 	Cause Critical 	Cause Light 	Cause Serious
+        # Harm 	Plague
+        # Conjuration
+        # Armor 	Augment Aura 	Channel Faith 	Create Food
+        # Create Spring 	Create Water 	Fountain 	Magic Carpet
+        # Chaos
+        # Gate Travel
+        # Abjuration
+        # Abjure 	Counterspell 	Cure Disease 	Cure Poison
+        # Protection From Evil 	Protection From Good 	Remove Curse 	Sanctuary
+        # Tremor
+        # Alteration
+        # Calm 	Change Sex 	Cure Blindness 	Cure Critical
+        # Cure Light 	Erase 	Heal 	Poison
+        # Refresh 	Resurrect
+        # Summoning
+        # Find Familiar 	Ice Storm 	Summon 	Summon Shade
+        # Thunderstorm 	Totem
+        # Enchantment
+        # Beacon 	Consecrate Food 	Curse 	Desecrate Armor
+        # Elemental Shield 	Faerie Fire
+        # Illusion
+        # Light
+        # Invocation
+        # Bless
+        # Evocation
+        # Call Lightning
+        # Divination
+        # Detect Illusion 	Detect Invisibility 	Identify 	Read Aura
+        # Sense Life
+        # Necromancy
+        # Animate Dead 	Cause Critical 	Cause Light 	Cause Serious
+        # Harm 	Plague 	Reanimate
+        # Conjuration
+        # Armor 	Channel Faith 	Create Food 	Create Spring
+        # Create Water 	Fountain 	Magic Carpet
+        # Chaos
+        # Gate Travel
+        # Evocation
+        # Air Evocation 	Ash Evocation 	Dust Evocation 	Earth Evocation
+        # Fire Evocation 	Ice Evocation 	Lightning Evocation 	Magma Evocation
+        # Minerals Evocation 	Ooze Evocation 	Radiance Evocation 	Salt Evocation
+        # Smoke Evocation 	Steam Evocation 	Vacuum Evocation 	Water Evocation
+        # Invocation
+        # Air Invocation 	Ash Invocation 	Dust Invocation 	Earth Invocation
+        # Fire Invocation 	Ice Invocation 	Lightning Invocation 	Magma Invocation
+        # Minerals Invocation 	Ooze Invocation 	Radiance Invocation 	Salt Invocation
+        # Smoke Invocation 	Steam Invocation 	Vacuum Invocation 	Water Invocation
+        # Abjuration
+        # Negation 	Pentacle 	Protection From Evil 	Protection From Good
+        # Remove Curse
+        # Alteration
+        # Calm 	Erase 	Poison 	Shadow Door
+        # Summoning
+        # Shadow Golem 	Shadow Imp 	Summon 	Summon Shade
+        # Swarm
+        # Enchantment
+        # Beacon 	Curse 	Hex
+        # Illusion
+        # Blindness
+        # Divination
+        # Detect Invisibility
+        # Necromancy
+        # Chill Touch 	Delay Reincarnation 	Disjunction 	Famine
+        # Fatigue 	Harm 	Immobilize 	Impede Movement
+        # Leech 	Leech Conduit 	Malediction 	Malignancy
+        # Plague 	Reanimate 	Shadow Armor
+        # Conjuration
+        # Convocation 	Magic Message 	Shadow Light
+        # Chaos
+        # Confusion 	Deafen 	Evil Eye 	Insanity
+        # Jinx
+        # ADVANCED
+        # Abjuration
+        # Antimagic Sphere
+        # Alteration
+        # Cure Mutilation 	Frenzy 	Lava Walk 	Magnetic Field
+        # Mutilate 	Pustulate 	Quicksand
+        # Summoning
+        # Megalith 	Monument 	Swarm 	Warp
+        # Enchantment
+        # Bathe 	Frostborne 	Lava Cloak
+        # Illusion
+        # Displacement
+        # Invocation
+        # Air Halo
+        # Evocation
+        # Chain Lightning 	Diseased Cloud 	Ice Whip 	Landslide
+        # Reflective Fireball 	Waterspout
+        # Divination
+        # Illumination
+        # Necromancy
+        # Necromancers Guile
+        # Conjuration
+        # Acid Mist 	Blood Dance 	Cursed Fog 	Ember Carom
+        # Lightning Shroud 	Poison Gas 	Soul Sacrifice
+        # Chaos
+        # Air Blast 	Buffet 	Damnation 	Withering Touch
 
 
         self.inventory = list()
